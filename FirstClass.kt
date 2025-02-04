94b60d2b3704861b2583bd910c8934f4d508cc56
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2025-02-04 10:48:42 +0400

    Create FirstClass.kt

---
+++ FirstClass.kt
@@ -0,0 +1,10 @@
+class FirstClassKt {
+    var name:String?= "Name is not defined"
+    var price:Int?= 0
+
+    fun showInfos(){
+        println("INFOS: ")
+        println("Name: $name")
+        println("Price: $price")
+    }
+}
