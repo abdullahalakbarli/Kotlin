5da2e7d4b74831fbfbc84e3249ec1b1f7c823d69
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2025-02-04 11:08:23 +0400

    Update FirstClass.kt

--- FirstClass.kt
+++ FirstClass.kt
@@ -4,7 +4,18 @@ class FirstClassKt {
 
     fun showInfos(){
         println("INFOS: ")
-        println("Name: $name")
-        println("Price: $price")
+        println("Name: ${name ?: "Ad yoxdur"}")
+        println("Price: ${price ?: "Qiymet yoxdur"}")
+    }
+    constructor(name:String, price:Int){
+        this.name = name
+        this.price = price
+    }
+    data class Car(var name: String, var price: Int){
+        fun showInfos(){
+            println("INFOS: ")
+            println("Name: ${name ?: "Ad yoxdur"}")
+            println("Price: ${price ?: "Qiymet yoxdur"}")
+        }
     }
 }
